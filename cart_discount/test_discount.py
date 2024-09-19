bbadbc95d1d6220a6841cda3e424c304aa97d148
Your Name <you@example.com>
2024-09-18 19:15:15 -0500

    Lab1, Part 1

--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -9,7 +9,31 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    
+    def test_list_of_four_prices(self):
+        prices = [10, 4, 20, 2]
+        expected_discount = 2
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_two_prices(self):
+        prices = [10, 2]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_with_empty_list(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_with_empty_list(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def discount_called_with_strings(self):
+        self.fail('finish this test')
+
+    def discount_called_with_float(self):
+        self.fail('finish this test')
     # TODO more unit tests here. Each test should test one scenario
 
 
